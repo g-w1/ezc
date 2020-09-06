c3fa4980ccfc94cb82ac837019d9e261a96da8dc
g-w1 <jacoblevgw@gmail.com>
2020-09-06 19:19:42 -0400

    add in main an example

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,11 @@
 #[warn(missing_debug_implementations)]
 pub mod lexer;
 
-fn main() {}
+fn main() {
+    let mut tokenizer = lexer::Tokenizer::new();
+    let op = tokenizer.lex(String::from("Set x to 10. Set y to 5."));
+    if let Ok(_) = op {
+        println!("{:?}", tokenizer.output);
+    }
+
+}
