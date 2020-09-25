903520040af546b6300529e19528873dca98e4c9
g-w1 <jacoblevgw@gmail.com>
2020-09-25 13:30:42 -0400

    add colors on errors

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,30 @@ pub mod parser;
 use std::env::args;
 use std::fs;
 
+const ERROR: &str = "\x1B[31mERROR: \x1B[0m";
+
 fn main() {
     let input;
     if let Some(filename) = args().nth(1) {
         input = match fs::read_to_string(&filename) {
             Ok(a) => a,
             Err(_) => {
-                eprintln!("Error: Cannot read file: `{}`.", filename);
+                eprintln!("{} Cannot read file: `{}`.", ERROR, filename);
                 std::process::exit(1);
             }
         }
     } else {
-        eprintln!("Error: I need the first command line arg to be the file to compile.");
+        eprintln!(
+            "{}I need the first command line arg to be the file to compile.",
+            ERROR
+        );
         std::process::exit(1);
     }
     // let input = "set z to 4. change z to 3. set x to z + z.";
     let mut tokenizer = lexer::Tokenizer::new();
     let output = tokenizer.lex(input);
     if let Err(e) = output.0 {
-        println!("{}", e);
+        println!("{}{}", ERROR, e);
         std::process::exit(1);
     }
     match parser::parse(output.0.unwrap(), output.1) {
@@ -39,12 +44,12 @@ fn main() {
                 println!("{}", code);
             }
             Err(e) => {
-                println!("{}", e);
+                println!("{}{}", ERROR, e);
                 std::process::exit(1);
             }
         },
         Err(e) => {
-            println!("{}", e);
+            println!("{}{}", ERROR, e);
             std::process::exit(1);
         }
     }
