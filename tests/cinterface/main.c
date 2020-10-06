abcf1cf039c3e32807f6e216bbcd8476623f93ea
g-w1 <jacoblevgw@gmail.com>
2020-10-06 15:19:01 -0400

    BRUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUH.
    i added one thing just playing around and now it works.  ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.

--- tests/cinterface/main.c
+++ tests/cinterface/main.c
@@ -2,6 +2,6 @@
 
 int main() {
   extern long long MaNgLe_another_test();
-  printf("%lld\n", MaNgLe_another_test(10, 11, 12));
+  printf("%lld\n", MaNgLe_another_test(10, 11, 121));
   return 0;
 }
