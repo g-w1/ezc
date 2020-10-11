cb5a57006bad3349c19f1507d6c71215b5f19ab4
g-w1 <jacoblevgw@gmail.com>
2020-10-10 22:35:58 -0400

    try unicode but need https://github.com/ziglang/zig/pull/6390 to be merged

--- lib/src/main.zig
+++ lib/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 export fn PutChar(c: u8) i64 {
     const stdout = std.io.getStdOut().outStream();
+    // TODO add unicode with {u} when https://github.com/ziglang/zig/pull/6390 is merged
     stdout.print("{c}", .{c}) catch return -1;
     return 0;
 }
